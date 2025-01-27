0fc7ace372da8de6ab0d79b1458edaddeb4ac5b9
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-27 23:25:08 +0200

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,52 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: "postgres",
+                host: configService.get("DB_HOST"),
+                port: Number(configService.get("DB_PORT")),
+                username: configService.get("DB_USERNAME"),
+                password: configService.get("DB_PASSWORD"),
+                database: configService.get("DB_NAME"),
+                synchronize: true,
+                autoLoadEntities: true,
+                entities: [
+                    /* CategoryEntity,
+                    ProductEntity,
+                    ProductEntity,
+                    AddressEntity,
+                    FavoriteEntity,
+                    UserEntity,
+                    OrderEntity,
+                    ReviewEntity,
+                    ColorEntity,
+                    BrandEntity,
+                    SizeEntity, */
+                ],
+            }),
+            inject: [ConfigService],
+        }),
+        /* CategoriesModule,
+        ProductsModule,
+        BrandsModule,
+        UsersModule,
+        AddressesModule,
+        AuthModule,
+        FavoritesModule,
+        OrdersModule,
+        ReviewsModule,
+        ColorsModule,
+        SizesModule, */
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
