1d30bcd3d8cc0ea17e3a2a0ea8caaf4d45fba713
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-28 14:18:23 +0200

    _

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    Request,
+    UseGuards,
+} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { LoginDto } from "./dto/login.dto";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
+import { ApiBearerAuth } from "@nestjs/swagger";
 
 @Controller("auth")
 export class AuthController {
@@ -11,5 +20,17 @@ export class AuthController {
     @UseGuards(LocalAuthGuard)
     async login(@Body() loginDto: LoginDto) {
         return await this.authService.login(loginDto);
+    }
+
+    /* @Post("register")
+    async register(@Body() loginDto: LoginDto) {
+        return await this.authService.register(loginDto);
+    } */
+
+    @Get("profile")
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    async getProfile(@Request() req: any) {
+        return this.authService.getProfile(req.user.email);
     }
 }
